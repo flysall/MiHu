3b1182a62ccbd7e1923245412a7576b08d75fdc0
flysall <flysallme@gmail.com>
2017-11-26 21:50:42 +0800

    upload the front end code

--- src/main/webapp/js/mask.js
+++ src/main/webapp/js/mask.js
@@ -1,65 +1,89 @@
 $(function(){
-    /*提问模态框处理*/
-    var openMask = $(".open-mask");
-    var closeMask = $(".close-mask");
-    var mask = $(".mask");
-    var askModal = $("#ask-modal");
+	/*提问模态框处理*/
+	var openMask = $(".open-mask");
+	var closeMask = $(".close-mask");
+	var mask = $(".mask");
+	var askModal = $("#ask-modal");
 
-    openMask.click(function(){
-        mask.show();
-        askModal.show();
-    });
+	openMask.click(function(){
+		mask.show();
+		askModal.show();
+	});
+	closeMask.click(function(){
+		mask.hide();
+		askModal.hide();
+	});
 
-    closeMask.click(function(){
-        mask.hide();
-        askModal.hide();
-    })
+	/*富文本编辑器创建*/
+	var editor = new wangEditor('questionContent');
 
-    /*富文本编辑器创建*/
-    var editor = new wangEditor('questionContent');
-    editor.config.menus = [
-        'bold',
-        'img',
-        'fullscreen',
-    ];
+	editor.config.menus = [
+		'bold',
+		'img',
+		'fullscreen',
+	];
 
-    //配置图片上传路径，最好为相对路径
-    editor.config.uploadImgUrl = 'uploadImage';
-    //配置图片上传到后台的参数名称
-    editor.config.uploadImgFileName = 'paramName';
-    editor.creat();
-
-    /*提问表单处理*/
-    var askButton = $("#askButton");
+	//配置处理图片上传的路径，最好用相对路径
+	editor.config.uploadImgUrl = 'uploadImage';
+	//配置图片上传到后台的参数名称
+	editor.config.uploadImgFileName = 'paramName';
+	editor.create();
+	
+	
+	/*提问表单处理*/
+	/* 提问处理 */
+	var askButton = $("#askButton");
     var askForm = $("#askForm");
     var questionTitleInput = $("#questionTitle");
     var questionContentTextarea = $("#questionContent");
     var topicNameStringInput = $("#topicNameString");
-
-    askButton.on("click", function(){
+	
+    askButton.on("click",function(){
         var formData = new FormData();
         var v1 = questionTitleInput.val();
-        var v2 = questionContentTextare.val();
+        var v2 = questionContentTextarea.val();
         var v3 = topicNameStringInput.val();
-        if(v1.trim() == "" || v2.trim() =="" || v3.trim() == ""){
-            return;
+        if(v1.trim()==""||v2.trim==""||v3.trim()==""){
+        	return;
         }
-        formData.append("questionTitle", v1);
-        formData.append("questionContent", v2);
-        formData.append("topicNameString", v3);
+        formData.append("questionTitle",v1);
+        formData.append("questionContent",v2);
+        formData.append("topicNameString",v3);
         $.ajax({
-            url: basepath + "ask",
-            type: "post",
-            data: formData,
-            processData: false,
-            contentType: false,
-            success: function(response){
-                if(response.state == 0){
-                    window.location.href = basepath + "question/" + response.data;
-                } else{
-                    alert("出现了错误!")
+            url:basepath + "ask",
+            type:"post",
+            data:formData,
+            processData:false,
+            contentType:false,
+            success:function(response){
+                if(response.state==0){
+                	window.location.href = basepath + "question/"+response.data;
+                }else{
+                	alert("出现了错误...");
                 }
             }
         });
     });
-})
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+});
